f391d699cfa10038811ea9675ea8d21b7e5ac1ae
ajay <ajay@Ajay>
2020-08-12 13:53:22 +0530

    WH-5280-First Time Commit.

---
+++ src/main/java/com/ajay/SpringBootGitHubAppApplication.java
@@ -0,0 +1,13 @@
+package com.ajay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGitHubAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGitHubAppApplication.class, args);
+	}
+
+}
