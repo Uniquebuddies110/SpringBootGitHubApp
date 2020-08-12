f17cc449fc9dce680e3bd9d543350ed46571dd7c
Uniquebuddies110 <45170941+Uniquebuddies110@users.noreply.github.com>
2020-08-12 14:34:15 +0530

    Update Message.java
    Added new Variable and renamed the text to text1

--- src/main/java/com/ajay/model/Message.java
+++ src/main/java/com/ajay/model/Message.java
@@ -2,23 +2,24 @@ package com.ajay.model;
 
 public class Message {
 
-	private String text;
+	private String text1;
+	private String text2;
 
 	public Message() {
 		super();
 	}
 
 	public String getText() {
-		return text;
+		return text1;
 	}
 
-	public void setText(String text) {
-		this.text = text;
+	public void setText(String text1) {
+		this.text1 = text1;
 	}
 
 	@Override
 	public String toString() {
-		return "Message [text=" + text + "]";
+		return "Message [text=" + text1 + "]";
 	}
 	
 	
