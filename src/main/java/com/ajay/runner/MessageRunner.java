ccc0477b0eb23f3acf59addfdaeaddefb8223c0f
ajay <ajay@Ajay>
2020-08-12 14:18:14 +0530

    WH-992 Modified Added 2nd Variable

--- src/main/java/com/ajay/runner/MessageRunner.java
+++ src/main/java/com/ajay/runner/MessageRunner.java
@@ -9,6 +9,7 @@ public class MessageRunner implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		int a=10;
+		int b=20;
 		System.out.println("Welcome to Modified Spring Boot Github App");
 	}
 
